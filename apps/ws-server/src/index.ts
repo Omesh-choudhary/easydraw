d23d47d12386247aef7ba669c4d51d21a94dd09f
omesh choudhary <omeshparswal@gmail.com>
2026-09-01 10:42:15 +0530

    minor changes

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,30 +1,71 @@
-import {WebSocketServer} from "ws"
+import {WebSocketServer, WebSocket} from "ws"
 import jwt from "jsonwebtoken"
 import {JWT_SECRET} from "@repo/common"
 const wss = new WebSocketServer({port:8080})
 
+ type room = {
+    [roomName:string] :
+        {
+            socket : WebSocket
+        }[]
+ }
 
-wss.on("connection", (ws, request)=>{
+const rooms:room ={}
+wss.on("connection", (ws)=>{
+    ws.on("message", (msg)=>{
 
-    const url = request.url
 
-    if (!url) {
-        ws.close()
-        return
-    }
+        const body = JSON.parse(msg.toString())
 
-    const queryParam = new URLSearchParams(url.split("?")[1])
-    const token = queryParam.get("token") || ""
-    const decoded = jwt.verify(token, JWT_SECRET || "")
 
-    if (!decoded) {
-        
-        ws.close()
-        return
-    }
+        if (body.type === "join-room") {
 
-    ws.on("message", (data)=>{
-        console.log("message received :", data.toString())
-        ws.send("hi")
+             if (!rooms[`${body.roomId}`]) {
+             rooms[body.roomId] = []
+           }
+
+           rooms[body.roomId]?.push({socket:ws})
+
+            
+        }
+
+        if (body.type === "leave-room") {
+
+            let leftUsers= rooms[body.roomId]?.filter(s=> s.socket !== ws)
+
+            if (leftUsers) {
+                rooms[body.roomId] = leftUsers
+            }
+            
+        }
+
+        if (body.type === "mouse-move") {
+        rooms[body.roomId]?.forEach((s) => {
+            if (s.socket !== ws && s.socket.readyState === WebSocket.OPEN) {
+            s.socket.send(JSON.stringify(body)); 
+            }
+        });
+        }
+
+       
+
     })
+
+    // const url = request.url
+
+    // if (!url) {
+    //     ws.close()
+    //     return
+    // }
+
+    // const queryParam = new URLSearchParams(url.split("?")[1])
+    // const token = queryParam.get("token") || ""
+    // const decoded = jwt.verify(token, JWT_SECRET || "")
+
+    // if (!decoded) {
+        
+    //     ws.close()
+    //     return
+    // }
+
 })
